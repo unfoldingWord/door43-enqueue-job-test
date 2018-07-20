1227218ea6cfed3104e7371405ad5dae4d060751
Robert Hunt <Robert.Hunt@DistantShores.org>
2018-07-20 15:44:58 -0400

    Fix missing docker logon

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+# Environment variables DOCKER_USERNAME and DOCKER_PASSWORD must be set
+
 # Parameter $1 is expected to be develop or master
+
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD ;
 
 if [ "$1" == "master" ]; then
     echo "Making MASTER image and pushing it" ;
