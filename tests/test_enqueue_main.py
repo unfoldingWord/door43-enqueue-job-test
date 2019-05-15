de18a96747e8b12d5eb36526f0d90005ac12cc74
Robert Hunt <Freely.Given.org@gmail.com>
2019-05-16 10:16:28 +1200

    Minor fixes to return value

--- tests/test_enqueue_main.py
+++ tests/test_enqueue_main.py
@@ -81,7 +81,7 @@ from redis import exceptions as redis_exceptions
             #self.assertEqual(response.status_code, 200)
             #self.assertEqual(response.headers['Content-Type'], 'application/json' )
             #response_dict = json.loads(response.data)
-            #self.assertEqual(response_dict['success'], 'true')
+            #self.assertEqual(response_dict['success'], True)
             #self.assertEqual(response_dict['status'], 'queued')
 
     #def test_webhook_with_typical_full_json_payload(self):
@@ -96,7 +96,7 @@ from redis import exceptions as redis_exceptions
             #self.assertEqual(response.status_code, 200)
             #self.assertEqual(response.headers['Content-Type'], 'application/json' )
             #response_dict = json.loads(response.data)
-            #self.assertEqual(response_dict['success'], 'true')
+            #self.assertEqual(response_dict['success'], True)
             #self.assertEqual(response_dict['status'], 'queued')
             ## After job has run, should update https://dev.door43.org/u/tx-manager-test-data/en-obs-rc-0.2/93829a566c/
 
